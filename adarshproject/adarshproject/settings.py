41c6652422bc350e947c010b1da3484d5d5baa13
Adarsh Singh <adarshsingh26899@gmail.com>
2026-08-15 01:02:49 +0530

    Prepare Django for deployment

--- adarshproject/adarshproject/settings.py
+++ adarshproject/adarshproject/settings.py
@@ -171,7 +171,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # =========================================================
 # MEDIA FILES
